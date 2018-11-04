a4146ab725180078b684917267546fc4b571ead8
bnguser <batngoal@gmail.com>
2018-11-04 11:35:13 +0530

    commit the code using try catch method

--- app.js
+++ app.js
@@ -2,8 +2,12 @@ const http = require('http');
 const fs = require('file-system');
 
 http.createServer(function(req,res){
-	
+	try{
 	let file =fs.readFileSync('.' +req['url'] + '.txt','utf8');
 	res.write(file);
-	res.end();
+}catch(error)
+{
+	res.write('404 file not found');
+	}
+res.end();
 }).listen(3000);
